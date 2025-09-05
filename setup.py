f35bfa3cf47413b6afc3c8bacd9664b9bb4f651b
houdhoudGH <nourgheffari@gmail.com>
2025-09-05 14:00:25 +0100

    package download

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot_llama2",
+    version="0.0.0",
+    author="Gheffari Nour El Houda",
+    author_email="nourgheffari@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
